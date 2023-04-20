a5c12a065e59370109ce7d09153815c2d64d8680
Arthur Aldrin Ramos <arthuraldrinramos@gmail.com>
2023-04-20 17:35:01 +0800

    sana matapos na

--- index.blade.php
+++ index.blade.php
@@ -34,4 +34,37 @@
                                     <label for="lastname">Last Name</label>
                                     <input type="text" class="form-control" id="lastname" name="lastname" value="{{ $user->lastname }}" readonly>
                                 </div>
-                                <div class
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <input type="text" class="form-control" id="address" name="address" value="{{ $user->address }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="city">City</label>
+                                    <input type="text" class="form-control" id="city" name="city" value="{{ $user->city }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="zip">Zip</label>
+                                    <input type="text" class="form-control" id="zip" name="zip" value="{{ $user->zip }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="birthdate">Birthdate</label>
+                                    <input type="date" class="form-control" id="birthdate" name="birthdate" value="{{ $user->birthdate }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="contact1">Contact 1</label>
+                                    <input type="text" class="form-control" id="contact1" name="contact1" value="{{ $user->contact1 }}" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="contact2">Contact 2</label>
+                                    <input type="text" class="form-control" id="contact2" name="contact2" value="{{ $user->contact2 }}" readonly>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endsection
